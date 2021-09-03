7a10042fff075fe21d48f71b52fdafa323ec68c3
pedroamenga <pedroam92@gmail.com>
2021-09-03 20:32:36 +0000

    counter

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,18 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+function SimpleCounter(props) {
+	return (
+		<div className="Contador">
+			<div className={"clock"}></div>;<div className={"four"}>0</div>;
+			<div className={"three"}>0</div>;<div className={"two"}>0</div>;
+			<div className={"one"}>0</div>;
+		</div>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(function() {
+	alert("Hello");
+}, 3000);
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
