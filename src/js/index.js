5172b83955abf3136550324c7ccd838f3ae3eccf
pedroamenga <pedroam92@gmail.com>
2021-09-05 23:52:19 +0000

    counterfinal

--- src/js/index.js
+++ src/js/index.js
@@ -7,21 +7,45 @@ import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
+import { PropTypes } from "prop-types";
 
 function SimpleCounter(props) {
 	return (
-		<div className="Contador">
+		<div className="counter">
 			<div>
 				<i className="far fa-clock"></i>
 			</div>
-			<div className="cuarto">0</div>;<div className="tercero">0</div>;
-			<div className="segundo">0</div>;<div className="primero">0</div>;
+			<div className="cuarto">{props.cuarto % 10}</div>;
+			<div className="tercero">{props.tercero % 10}</div>;
+			<div className="segundo">{props.segundo % 10}</div>;
+			<div className="primero">{props.primero % 10}</div>;
 		</div>
 	);
 }
 
+SimpleCounter.propTypes = {
+	primero: PropTypes.number,
+	segundo: PropTypes.number,
+	tercero: PropTypes.number,
+	cuarto: PropTypes.number
+};
+
+let counter = 0;
 //render your react application
 setInterval(function() {
-	alert("Hello");
-}, 3000);
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+	const cuatro = Math.floor(counter / 1000);
+	const tres = Math.floor(counter / 100);
+	const dos = Math.floor(counter / 10);
+	const uno = Math.floor(counter / 1);
+	counter++;
+
+	ReactDOM.render(
+		<SimpleCounter
+			primero={uno}
+			segundo={dos}
+			tercero={tres}
+			cuarto={cuatro}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
